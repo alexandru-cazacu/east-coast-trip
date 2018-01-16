d5815dde717f97650154159ebe8cb8d871d1c4f0
Alexandru Cazacu <alexandru.cazacu.9889@gmail.com>
2018-01-16 22:25:22 +0100

    Updated Landing page

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+var number = 0;
+
+$(document).ready(function () {
+    setInterval(doStuff, 1000);
+});
+
+function doStuff() {
+    $('.hero').css('background', 'url(images/hero' + number++ + '.jpg)');
+    var num = number - 1;
+    if ($('bullets .bullet:nth-child(' + num + ')').hasClass("active", function () {
+
+        })) {
+
+    }
+    $('.bullets .bullet:nth-child(' + num + ')').toggleClass("active");
+    if (number >= 7) {
+        number = 1;
+    }
+}
